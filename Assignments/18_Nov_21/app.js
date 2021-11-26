a8ffb2fa618be921bad70cc415ac4080674de469
AkshaykumarG26 <akshayg2697@gmail.com>
2021-11-26 10:33:06 +0530

    .

--- Assignments/18_Nov_21/app.js
+++ Assignments/18_Nov_21/app.js
@@ -2,28 +2,74 @@ const express = require("express");
 
 const app = express()
 
+const books = require("./books.json");
 
-app.get("/", (req,res) => {
-    res.send("hello")
+
+app.use(express.json())
+
+// app.use(logger)
+
+
+
+app.get("/", (req,res, next) => {
+    // const newUser = [...books, req.body]
+    // const postIt = res.send(newUser)
+    
+    //   or    //
+    // next()
+    res.send({books})
+
+});
+
+
+app.get("/books/:id", (req, res) => {
+    // const newBooks = req.params.id
+    // res.send({books})
+    const newBooks = books.filter((book)=> book.id === req.params.id)
+    res.send(newBooks)
+    // console.log(newBooks)
+})
+
+app.post("/books", (req, res) => {
+    const newUser = [...books, req.body]
+    res.send(newUser)
 })
 
 
 
 
 
+app.patch("/:id", (req, res) => {
+    const newBooks = books.map((book) => {
+        if(req.params.id === book.id){
+            if(req?.body?.id) book.id = req.body.id;
+            if(req?.body?.author) book.author = req.body.author;
+            if(req?.body?.book_name) book.book_name = req.body.book_name;
+            if(req?.body?.pages) book.pages = req.body.pages;
+            if(req?.body?.published_year) book.published_year = req.body.published_year;
+        }
+        return book;
+    })
+    res.send(newBooks)
+})
 
 
 
+app.delete("/books/:id", (req, res) => {
+    const newBooks = books.filter((book) => book.id !== req.params.id)
+    res.send(newBooks)
+})
 
 
-
-
-
+// function logger(req, res, next){
+//     res.send({"Requestd by": "Akshaykumar"})
+//     next()
+// }
 
 
 
 
 app.listen(1324, function(){
-    console.log("server running on port 1324")
+    console.log("server running on port 1324 Assignment of 18_Nov_21")
 
 })
--- Practicals/18_Nov_21/app.js
+++ Practicals/18_Nov_21/app.js
@@ -11,22 +11,44 @@ app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send({ users });
+
 });
 
+
 app.post("/", (req,res) => {
+    // console.log(req.body)
     const newUser = [...users, req.body];
-    res.send(newUser);
-    // return newUser
+    res.send(newUser)
+    
+})
+
+
+app.patch('/:email', (req, res) => {
+    const newUser = users.map((user) => {
+
+        if(req.params.email === user.email){
+            if(req?.body?.first_name) user.first_name = req.body.first_name;
+            if(req?.body?.last_name) user.last_name = req.body.last_name;
+            if(req?.body?.email) user.email = req.body.email;
+            if(req?.body?.gender) user.gender = req.body.gender;
+        }
+        return user;
+    })
+    res.send(newUser)
 })
 
 
 
-app.patch('/:id', (req, res) => {
-
+app.delete("/:email", (req, res) => {
+    const newUser = users.filter(user => user.email !== req.params.email);
+    res.send(newUser)
 })
 
 
-
+app.get("/:email", (req, res) => {
+    const newUser = users.filter(user => user.email === req.params.email);
+    res.send(newUser)
+})
 
 
 
@@ -36,5 +58,5 @@ app.patch('/:id', (req, res) => {
 
 
 app.listen(2345, function(){
-    console.log("we r listening on port 2345")
+    console.log("we r listening on port 2345 Practical on 18_Nov")
 });
