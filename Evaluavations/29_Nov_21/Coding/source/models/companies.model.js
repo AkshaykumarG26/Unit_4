1554cca8a4afb56feec86d1db725d650cf2fdb3a
AkshaykumarG26 <akshayg2697@gmail.com>
2021-11-29 17:02:26 +0530

    .

--- Evaluavations/29_Nov_21/Coding/source/models/companies.model.js
+++ Evaluavations/29_Nov_21/Coding/source/models/companies.model.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
-// const jobSchema = new mongoose.Schema({
-//     city: {type: String, required: true},
-//     location: {type: String, required: true},
-//     notoce_period: {type: Number, required: true},
-//     skills: {type: String, required: true},
-//     work_from_home: {type: String, required: false, default: "yes"},
-//     num_of_jobs: {type: Number, required: true}
-// });
+const jobSchema = new mongoose.Schema({
+    city: {type: String, required: true},
+    location: {type: String, required: true},
+    notoce_period: {type: Number, required: true},
+    skills: {type: String, required: true},
+    work_from_home: {type: String, required: false, default: "yes"},
+    num_of_jobs: {type: Number, required: true}
+});
 
 module.exports = jobSchema;
