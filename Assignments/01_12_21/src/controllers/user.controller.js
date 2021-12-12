1dbecb4e9113f361608493c9291140713771d50e
AkshaykumarG26 <akshayg2697@gmail.com>
2021-12-12 10:51:23 +0530

    .

--- Assignments/01_12_21/src/controllers/user.controller.js
+++ Assignments/01_12_21/src/controllers/user.controller.js
@@ -36,7 +36,7 @@ router.patch("/:id", upload.single("profile_pic"), async (req, res) => {
         
         const updateUser = await User.findByIdAndUpdate(req.params.id, {
             profile_pic: req.file.path
-        })
+        }).lean().exec();
         return res.status(201).json({ user })
     }catch(e){
         return res.status(500).json({ status: "failed", message: e.message})
@@ -44,6 +44,15 @@ router.patch("/:id", upload.single("profile_pic"), async (req, res) => {
 })
 
 
+
+router.delete("/:id", async(req, res) => {
+    try{
+        const user = await User.findByIdAndRemove(req.params.id).lean().exec();
+        return res.send(user)
+    }catch(e){
+        return res.status(500).json({ status: "failed", message: e.message})
+    }
+})
 
 
 
